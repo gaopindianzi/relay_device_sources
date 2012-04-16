1b2367b3d2744a6da0e94ea364248eaf96161d6a
Lzf <4366955@qq.com>
2012-04-16 13:10:19 +0800

    modbus输入状态没有调通。。。。错乱

--- debug.h
+++ debug.h
@@ -1,7 +1,7 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
-#define ASSERT(assert)   if(THISASSERT) do { if(!assert) { printf("assert failed at:%s,%s:%d\r\n",__FILE__,__FUNCTION__,__LINE__); NutSleep(10000); } }while(0)
+#define ASSERT(assert)   if(THISASSERT) do { if(!(assert)) { while(1) { printf("assert failed at:%s,%s:%d\r\n",__FILE__,__FUNCTION__,__LINE__); NutSleep(10000);} } }while(0)
 #define DEBUGMSG(on,str) if(on) do { printf str ; }while(0)
 
 
--- modbus_interface.c
+++ modbus_interface.c
@@ -50,10 +50,12 @@
 #include "debug.h"
 
 #define THISINFO        1
-#define THISERROR       1
-#define THISASSERT      1
+#define THISERROR       0
+#define THISASSERT      0
+#define DUMP_DATA_INFO  1
 
 
+#define BITS_TO_BS(bit_num)    ((bit_num+7)/8)
 
 
 int  ReadCoilStatus(uint8_t * pmodbus,unsigned int start,unsigned int number)
@@ -68,33 +70,32 @@ int  ReadCoilStatus(uint8_t * pmodbus,unsigned int start,unsigned int number)
 	ASSERT(tmp);
 	rc = _ioctl(_fileno(iofile), IO_OUT_GET, io_out_buffer);
 	fclose(iofile);
-	if(rc) {
-		DEBUGMSG(THISERROR,("Read Coil Status rc not valid!\r\n"));
-		return 0;
-	}
-	if(((start+number)/8) > tmp/8) {
-		//在范围外
-		DEBUGMSG(THISINFO,("Read Coil status param error:%d,%d\r\n",start,number));
-		return 0;
-	} else {
+	ASSERT(rc==0);
+
+	if(number) {
 		//范围内
 		unsigned char B,b;
 		unsigned int i;
 		B = start/8;b = start % 8;
-		DEBUGMSG(THISINFO,("Read coil s,n(%d,%d)\r\n",start,number));
+		DEBUGMSG(THISINFO,("Read coil start=%d,number=%d\r\n",start,number));
 		for(i=0;i<number;i++) {
-			pmodbus[1+i] <<= 1;
-			pmodbus[1+i] |= (io_out_buffer[B] & (1<<b))?1:0;
+			unsigned char msk = 1<<(i%8);
+			pmodbus[1+i/8] <<= 1;
+			if(B < BITS_TO_BS(tmp)) {
+			    pmodbus[1+i/8] |= (io_out_buffer[B] & (1<<b))?msk:0;
+			} else {
+				pmodbus[1+i/8] &= ~msk;
+			}
 			if(++b >= 8) {
 				b = 0;
 				++B;
 			}
-			if((start+i) >= number) {
-				break;
-			}
 		}
-		pmodbus[0] = ((i<8)?1:(i/8+1));
-		return 1 + pmodbus[0];
+		//这里存放的是modbus/tcp数据的长度，等于字节的长度加上1
+		pmodbus[0] = BITS_TO_BS(number); 
+		//pmodbus[0] = (pmodbus[0]<2)?2:pmodbus[0];
+		//pmodbus[0] += 1; //加上number_bytes的占位一个字节
+		return 3 + pmodbus[0]; //返回发送的字节+head,1字节是数量位符,1字节是地址,1字节是功能码
 	}
 }
 
@@ -105,12 +106,8 @@ int  ForceSingleCoil(uint8_t * pmodbus,unsigned int start,unsigned char onoff)
 	FILE * iofile = fopen("relayctl", "w+b");
 	ASSERT(iofile);
 	rc = _ioctl(_fileno(iofile), GET_OUT_NUM, &tmp);
-	if(start/8 > tmp/8) {
-		//在范围外
-		DEBUGMSG(THISINFO,("ForceSingleCoil param error:%d,%d\r\n",start,onoff));
-		fclose(iofile);
-		return 0;
-	} else {
+	ASSERT(rc==0);
+	{
 		//范围内
 		unsigned char buffer[2];
 		buffer[1] = start >> 8;
@@ -133,7 +130,7 @@ int prase_modbus_protocol(TCPSOCKET * sock,char * pbuf,unsigned int len)
 	modbus_tcp_head * phead = (modbus_tcp_head *)pbuf;
 	uint8_t  * pmodbus = (uint8_t *)(pbuf+sizeof(modbus_tcp_head));
 
-	dumpdata(pbuf,len);
+	if(DUMP_DATA_INFO)dumpdata(pbuf,len);
 
 	//判断是否modbus/tcp协议
 	if(len < sizeof(modbus_tcp_head)) {
@@ -148,7 +145,7 @@ int prase_modbus_protocol(TCPSOCKET * sock,char * pbuf,unsigned int len)
 		{
 			DEBUGMSG(THISINFO,("modbus 0x01:\r\n"));
 			if(len < 4) {
-				DEBUGMSG(THISINFO,("len < 4 error\r\n"));
+				DEBUGMSG(THISERROR,("len < 4 error\r\n"));
 				break;
 			} else {
 				unsigned int start,number;
@@ -158,11 +155,12 @@ int prase_modbus_protocol(TCPSOCKET * sock,char * pbuf,unsigned int len)
 				len = ReadCoilStatus(pmodbus,start,number);
 				if(len > 0) {
 					DEBUGMSG(THISINFO,("ReadCoilStatus Ok len(%d)\r\n",len));
+					//len += 1; //加上1字节的
 					phead->lengthl = len & 0xFF;
 					phead->lengthh = len >> 8;
-					return len + sizeof(modbus_tcp_head);
+					return len + sizeof(modbus_tcp_head) - 2;
 				} else {
-					DEBUGMSG(THISINFO,("ReadCoilStatus Ko\r\n"));
+					DEBUGMSG(THISERROR,("ReadCoilStatus Ko\r\n"));
 				}
 			}
 		}
@@ -171,7 +169,7 @@ int prase_modbus_protocol(TCPSOCKET * sock,char * pbuf,unsigned int len)
 		{
 			DEBUGMSG(THISINFO,("modbus 0x05:\r\n"));
 			if(len < 4) {
-				DEBUGMSG(THISINFO,("len error!\r\n"));
+				DEBUGMSG(THISERROR,("len error!\r\n"));
 			} else {
 				unsigned int start,onoff;
 				start = pmodbus[0]; start <<= 8; start |= pmodbus[1];
@@ -182,7 +180,7 @@ int prase_modbus_protocol(TCPSOCKET * sock,char * pbuf,unsigned int len)
 					DEBUGMSG(THISINFO,("ForceSingleCoil Ok,len(%d)\r\n",len));
 					return len + sizeof(modbus_tcp_head);
 				} else {
-					DEBUGMSG(THISINFO,("ForceSingleCoil Ko\r\n"));
+					DEBUGMSG(THISERROR,("ForceSingleCoil Ko\r\n"));
 				}
 			}
 		}
@@ -211,7 +209,7 @@ THREAD(modbus_thread, arg)
 		if(NutTcpAccept(sock,502)) {
 			//等待接收超时，从新打开连接
 			NutTcpCloseSocket(sock);
-			DEBUGMSG(THISINFO,("NutTcpAccept(sock) failed!\r\n"));
+			DEBUGMSG(THISERROR,("NutTcpAccept(sock) failed!\r\n"));
 			continue;
 		}
 		DEBUGMSG(THISINFO,("Get One Connectting...\r\n"));
@@ -219,15 +217,15 @@ THREAD(modbus_thread, arg)
 			char buff[128];
 			int len = NutTcpReceive(sock,buff,sizeof(buff));
             if(len == 0) {
-				DEBUGMSG(THISINFO,("Tcp Recieve timeout.\r\n"));
+				DEBUGMSG(THISERROR,("Tcp Recieve timeout.\r\n"));
 				//经过测试，把网线拔掉再插上，只会出现超时的异常，系统不会出问题。
 				//所以，超时的时候，也必须退出连接
 				break;
 			} else if(len == -1) {
 				int error = NutTcpError(sock);
-				DEBUGMSG(THISINFO,("CMD:Tcp Receive ERROR(%d)\r\n",error));
+				DEBUGMSG(THISERROR,("CMD:Tcp Receive ERROR(%d)\r\n",error));
 				if(error == ENOTCONN)  {
-					DEBUGMSG(THISINFO,("CMD:Socket is not connected,break connecting\r\n"));
+					DEBUGMSG(THISERROR,("CMD:Socket is not connected,break connecting\r\n"));
 					break;
 				} else {
 					DEBUGMSG(THISERROR,("CMD:Socket is unknow error,break connecting\r\n"));
@@ -239,14 +237,14 @@ THREAD(modbus_thread, arg)
 				if(len > 0) {
 					//
 					DEBUGMSG(THISINFO,("Ack Data:%d\r\n",len));
-					dumpdata(buff,len);
+					if(DUMP_DATA_INFO)dumpdata(buff,len);
 					NutTcpSend(sock,buff,len);
 				}
 			}
 		}
 		NutTcpCloseSocket(sock);
 	}
-	DEBUGMSG(THISINFO,("modbus thread is running...\r\n"));
+	DEBUGMSG(THISINFO,("modbus thread is exit.\r\n"));
 }
 
 void StartModbus_Interface(void)
