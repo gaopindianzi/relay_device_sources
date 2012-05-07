e74810214a4380980fb376acd731226a1efef0ab
Lzf <4366955@qq.com>
2012-05-08 03:18:30 +0800

    定义了数据结构

---
+++ multimgr_device.c
@@ -0,0 +1,56 @@
+
+#include <compiler.h>
+#include <cfg/os.h>
+
+#include <string.h>
+#include <io.h>
+#include <fcntl.h>
+#include <time.h>
+
+#include <dev/board.h>
+#include <dev/urom.h>
+#include <dev/nplmmc.h>
+#include <dev/sbimmc.h>
+#include <fs/phatfs.h>
+
+#include <sys/version.h>
+#include <sys/thread.h>
+#include <sys/timer.h>
+#include <sys/heap.h>
+#include <sys/confnet.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+#include <net/route.h>
+#include <netdb.h>
+
+#include <pro/httpd.h>
+#include <pro/dhcp.h>
+#include <pro/ssi.h>
+#include <pro/asp.h>
+#include <pro/discover.h>
+
+#include <dev/watchdog.h>
+#include <sys/timer.h>
+#include <dev/ds1307rtc.h>
+#include <dev/rtc.h>
+#include <time.h>
+
+#ifdef NUTDEBUG
+#include <sys/osdebug.h>
+#include <net/netdebug.h>
+#endif
+
+#include "bsp.h"
+#include "sysdef.h"
+
+#include "des.h"
+#include "multimgr_device.h"
+
+#include "debug.h"
+
+#define THISINFO        0
+#define THISASSERT      1
+
+
+
